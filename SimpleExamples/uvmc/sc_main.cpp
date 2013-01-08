48175ed8430021d15916ac04378c64f28ea18da6
Mark Carey <mark_carey@mentor.com>
2013-01-08 15:41:46 +0000

    modified to work with Questa 10.1d

--- SimpleExamples/uvmc/sc_main.cpp
+++ SimpleExamples/uvmc/sc_main.cpp
@@ -1,3 +1,5 @@
+#ifndef __EDG__ 
+
 //v2: comments beginning with v2 are generated and used by V2
 //v2: please don't remove or modify these comments
 
@@ -10,8 +12,10 @@
 #include <uvmc.h>
 using namespace uvmc;
 
-int sc_main(int argc, char *argv[]) {
+#endif
 
+int sc_main(int argc, char *argv[]) {
+#ifndef __EDG__ 
 //v2: begin of channel declarations section
 //v2: end of channel declarations section
 
@@ -29,5 +33,7 @@ top *inst_top = new top("top");
  uvmc_connect(inst_top->ip->getPV()->master,"sv_in");
 
  sc_start(-1);
+#endif
  return 0;
 }
+
