81169f948d28926ce89f19bb231afc8c3d3daaca
Jon McDonald <jonmcdonald@gmail.com>
2013-01-11 09:00:49 -0500

    Added ARM m3 example

---
+++ ARM/m3/CBwork/demo/demo.c
@@ -0,0 +1,105 @@
+
+#include <stdio.h>
+#include <stdint.h>
+#include <math.h>
+#include "nvic.h"
+
+#define hw_addr(x)	(volatile unsigned long*)(x)
+#define hw_read(x)	*hw_addr(x)
+#define hw_write(x,y)	*hw_addr(x)=(y)
+
+#define FF		0x40000000
+#define FFSTATUS	(FF+0x40)
+#define FFCOUNT		(FF+0x44)
+#define FFCLRIRQ	(FF+0x50)
+#define FFFULL		0x3A
+#define FFEMPTY		0x05
+
+#define IOSINK          0x40000100
+#define IOCNT           0x40000200
+#define IOSTATUS        (IOCNT+0x4)
+
+#define true 1
+#define false 0
+
+int ffFull;
+
+int isPrime(int num) {
+  if (num <= 1)
+    return false;
+  else if (num == 2)
+    return true;
+  else if (num % 2 == 0)
+    return false;
+  else{
+    int prime = true;
+    int divisor = 3;
+    double md = sqrt(55.5);
+    while ((divisor * divisor) < num) {
+      if (num % divisor == 0) {
+        prime = false;
+        break; }
+      divisor += 2; }
+    return prime; }
+}
+
+int GetNextPrimes(int n, int pcount) {
+  int havePrime;
+  int i;
+  int p = 0;
+
+  while (p++ < pcount) {
+    havePrime = false;
+    i = n;
+    while (!havePrime) {
+      i++;
+      havePrime = isPrime(i);
+    }
+    n = i+1;
+  }
+  return i;
+}
+
+int main () {
+  int i;
+  int c;
+
+  hw_write(IOSTATUS, 1);
+  //printf ("Starting main\n");
+  ffFull = false;
+
+  i = GetNextPrimes(10000000, 100);
+  hw_write(IOSTATUS, 2);
+  printf ("Prime is %d\n", i);
+
+  /*asm volatile ("cpsie i");*/
+  NVIC_EnableIRQ(1);
+
+  while (true) {
+
+    while (!ffFull) {
+      //printf ("Called wfi\n");
+      hw_write(IOSTATUS, 0);
+      asm volatile("wfi"); 
+      hw_write(IOSTATUS, 1);
+    }
+  
+    c = hw_read(FFCOUNT);
+    while (c != 0) {
+      i = hw_read(FF);
+      c = hw_read(FFCOUNT);
+      //printf ("Read i = %d\n", i);
+    }
+    //printf ("Finished read loop\n");
+    ffFull = false;
+  }
+
+  //printf ("Returning from main\n");
+  return 0;
+}
+
+void __attribute__ ((interrupt)) __cs3_isr_external_1(void) {
+    ffFull = true;
+    hw_write(FFCLRIRQ, 0);
+    //printf ("        Called isr 1\n");
+}
---
+++ ARM/m3/main.cpp
@@ -0,0 +1,26 @@
+#include "simple.h"
+#include <time.h>
+
+#define TOP simple
+
+int sc_main(int argc, char *argv[]) {
+
+  //sc_report_handler::set_actions("/IEEE_Std_1666/deprecated", SC_DO_NOTHING);
+
+  TOP *inst_top_arm = new TOP("Top");
+
+  clock_t TimeStart = clock ();
+
+
+  sc_start(-1);
+
+  clock_t TimeStop = clock ();
+
+  delete inst_top_arm;
+
+  cout << endl << "CPU time: " 
+       << difftime (TimeStop, TimeStart) / CLOCKS_PER_SEC << " sec" 
+       << endl << endl;
+
+  return 0;
+}
