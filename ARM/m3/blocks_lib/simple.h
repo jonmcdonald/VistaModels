c4574587381b265554ccd7264d26541fd42d01b5
Jon McDonald <jonmcdonald@gmail.com>
2013-01-14 11:31:18 -0500

    Added UART to M3 Design

--- ARM/m3/blocks_lib/simple.h
+++ ARM/m3/blocks_lib/simple.h
@@ -6,8 +6,8 @@ $includes_begin;
 #include "../Models/m3Bus_model.h"
 #include "../Models/M3_model.h"
 #include "../Models/mem_model.h"
-#include "../Models/sink_model.h"
 #include "../Models/extIn_model.h"
+#include "Uart.h"
 $includes_end;
 
 $module_begin("simple");
@@ -28,11 +28,11 @@ $end
 $init("bus_inst"),
 bus_inst(0)
 $end
-$init("sink_inst"),
-sink_inst(0)
-$end
 $init("extIn_inst"),
 extIn_inst(0)
+$end
+$init("uart_inst"),
+uart_inst(0)
 $end
     $initialization_end
 {
@@ -49,11 +49,11 @@ $end;
 $create_component("bus_inst");
 bus_inst = new m3Bus_pvt("bus_inst");
 $end;
-$create_component("sink_inst");
-sink_inst = new sink_pvt("sink_inst");
-$end;
 $create_component("extIn_inst");
 extIn_inst = new extIn_pvt("extIn_inst");
+$end;
+$create_component("uart_inst");
+uart_inst = new Uart("uart_inst");
 $end;
 $bind("cpu_inst->dcode","bus_inst->m1");
 vista_bind(cpu_inst->dcode, bus_inst->m1);
@@ -70,17 +70,14 @@ $end;
 $bind("cpu_inst->icode","bus_inst->m0");
 vista_bind(cpu_inst->icode, bus_inst->m0);
 $end;
-$bind("superfifo->irq","cpu_inst->irq_1");
-vista_bind(superfifo->irq, cpu_inst->irq_1);
-$end;
 $bind("extIn_inst->y","bus_inst->m3");
 vista_bind(extIn_inst->y, bus_inst->m3);
 $end;
-$bind("bus_inst->s2","sink_inst->a");
-vista_bind(bus_inst->s2, sink_inst->a);
-$end;
 $bind("bus_inst->s3","extIn_inst->a");
 vista_bind(bus_inst->s3, extIn_inst->a);
+$end;
+$bind("bus_inst->s2","uart_inst->slave_1");
+vista_bind(bus_inst->s2, uart_inst->slave_1);
 $end;
     $elaboration_end;
   $vlnv_assign_begin;
@@ -88,6 +85,10 @@ m_library = "blocks_lib";
 m_vendor = "";
 m_version = "";
   $vlnv_assign_end;
+  superfifo->irq(cpu_inst->irq_1);
+  uart_inst->UARTTXINTR(cpu_inst->irq_2);
+  uart_inst->UARTRXINTR(cpu_inst->irq_3);
+
   }
   ~simple() {
     $destructor_begin;
@@ -103,11 +104,11 @@ $end;
 $destruct_component("bus_inst");
 delete bus_inst; bus_inst = 0;
 $end;
-$destruct_component("sink_inst");
-delete sink_inst; sink_inst = 0;
-$end;
 $destruct_component("extIn_inst");
 delete extIn_inst; extIn_inst = 0;
+$end;
+$destruct_component("uart_inst");
+delete uart_inst; uart_inst = 0;
 $end;
     $destructor_end;
   }
@@ -125,11 +126,11 @@ $end;
 $component("bus_inst");
 m3Bus_pvt *bus_inst;
 $end;
-$component("sink_inst");
-sink_pvt *sink_inst;
-$end;
 $component("extIn_inst");
 extIn_pvt *extIn_inst;
+$end;
+$component("uart_inst");
+Uart *uart_inst;
 $end;
   $fields_end;
   $vlnv_decl_begin;
