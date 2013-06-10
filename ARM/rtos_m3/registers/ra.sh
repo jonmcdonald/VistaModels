ddf2d1002051115a327a792fbc45947bdbd339a3
Mark Carey <mark_carey@mentor.com>
2013-06-10 13:48:33 +0100

    new register assistant files

---
+++ ARM/rtos_m3/registers/ra.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+export RA_HOME=/store/tools/HDS_2012.1/registerassistant
+
+echo "Block Name,Project Parameter Name,Project Parameter Value,Project Parameter Description" > input/C_settings.csv
+echo -n "reg_block,c.TEMPLATE_PATH," >> input/C_settings.csv
+PWD=`pwd`
+echo -n `./path_convert.sh $RA_HOME/resources/templates $PWD/input/c_header.h` >> input/C_settings.csv
+echo ",Path to the C header template." >> input/C_settings.csv
+
+$RA_HOME/regassist -project ./output -f ./ctrl.rcf > logfile.txt 
+cat logfile.txt
