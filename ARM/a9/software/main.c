f07b82b5e04d2df2620d307d60571aa49b3d7446
Mark Carey <mark_carey@mentor.com>
2013-10-15 14:22:35 +0100

    added a semi host print statement

--- ARM/a9/software/main.c
+++ ARM/a9/software/main.c
@@ -2,6 +2,8 @@
  
 #include <stdint.h>
 
+#include "mb/sw/control.h"
+
 #include "lcd.h"
 #include "examples/ui.h"
 
@@ -12,6 +14,9 @@ void kernel_main(uint32_t r0, uint32_t r1, uint32_t atags) {
     UNUSED(r0);
     UNUSED(r1);
     UNUSED(atags);
+
+    // example of semi hosting print message
+    mb_core_message("*** ARM A9 OpenGL Platform ***");
 
     init_lcd();
   
