08f56a3a75ad0f5ba53a708f17da9fbf25e385b8
Mark Carey <mark_carey@mentor.com>
2014-03-13 16:56:14 +0000

    fixed order so clean is done upfront

--- ARM/linux_qt_a9/sw/script/build_axf.sh
+++ ARM/linux_qt_a9/sw/script/build_axf.sh
@@ -8,8 +8,9 @@ source $SW_ROOT/script/setup_kernel.sh
 
 cd $SW_ROOT/boot
 
+make clean
+
 cat ../packages/linux-$VER_LINUX/arch/arm/boot/zImage ../packages/linux-$VER_LINUX/arch/arm/boot/dts/vista.dtb | dd of=zImage bs=4 conv=sync
 
-make clean
 make FILESYSTEM=../ramdisk/initrd.cpio.gz CROSS_COMPILE=$CROSS_COMPILE
 
