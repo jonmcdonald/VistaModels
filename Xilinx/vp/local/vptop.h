4d885f3887883040ca652ab03212e480f4f52dfe
Mark Carey <mark_carey@mentor.com>
2014-11-12 16:10:16 +0000

    updated to release version

--- Xilinx/vp/local/vptop.h
+++ Xilinx/vp/local/vptop.h
@@ -2,7 +2,7 @@
 #include "mgc_vista_schematics.h"
 $includes_begin;
 #include <systemc.h>
-#include "../../zynq/blocks_lib/Zynq_SoC.h"
+#include "../../zynq/zynq_schematics/Zynq_SoC.h"
 #include "../../pyex/models/AXI_GPIO_model.h"
 #include "../../pyex/models/CustomPeripheral_model.h"
 $includes_end;
