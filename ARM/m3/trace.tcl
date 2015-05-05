51839fcbdaee805c62ab11847f4b3be0db05ae1f
Jon McDonald <jonmcdonald@gmail.com>
2015-05-05 09:07:31 -0400

    Updates to m3 design using NIT

--- ARM/m3/trace.tcl
+++ ARM/m3/trace.tcl
@@ -4,3 +4,21 @@ set current_core "Top.cpu_inst.PV.core"
 add_default_symbol_file
 
 trace_function_activity * -kind eff
+trace_function_calls -kind eff
+
+#start_profiling profiling
+#enable_profiling -all
+trace_attribute Top.cpu_inst.PV.core.core_state -kind eff
+
+trace_socket Top.cpu_inst.icode -kind eff
+trace_socket Top.cpu_inst.dcode -kind eff
+trace_socket Top.cpu_inst.system -kind eff
+trace_socket Top.cpu_inst.irq_1 -kind eff
+
+trace_register Top.aes_inst.PV.Source -kind eff
+trace_register Top.aes_inst.PV.Destination -kind eff
+trace_register Top.aes_inst.PV.Size -kind eff
+trace_register Top.aes_inst.PV.status -kind eff
+trace_register Top.aes_inst.PV.clrIRQ -kind eff
+
+enable_coverage -design AEShw.dgn -test AEShw.tst
