90aed0a97b61bb01c061ee0380105c9bc0ba4408
Mark Carey <mark_carey@mentor.com>
2016-06-03 12:52:23 +0100

    removed dependency on filesys

--- ARM/a7/sw/boot-wrapper/build.sh
+++ ARM/a7/sw/boot-wrapper/build.sh
@@ -2,15 +2,10 @@
 
 LINUX=../linux-$VER_LINUX
 PLATFORM=A7
-FILESYSTEM=$1
-
-if [ -z "$FILESYSTEM" ]; then
-  FILESYSTEM=small_vts.cpio.gz
-fi
 
 make clean PLATFORM=$PLATFORM
 
 mkdir -p $PLATFORM
 cat $LINUX/arch/arm/boot/zImage $LINUX/arch/arm/boot/dts/vista.dtb | dd of=$PLATFORM/zImage bs=4 conv=sync
  
-make PLATFORM=$PLATFORM FILESYSTEM=$FILESYSTEM CROSS_COMPILE=$CROSS_COMPILE
+make PLATFORM=$PLATFORM CROSS_COMPILE=$CROSS_COMPILE
