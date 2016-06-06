9c7ba903b9314285e2b36e345712b4337cc87639
Mark Carey <mark_carey@mentor.com>
2016-06-06 12:44:01 +0100

    made linux into SMP added another UART

--- ARM/a7/hw/schematics/top.h
+++ ARM/a7/hw/schematics/top.h
@@ -5,11 +5,11 @@ $includes_begin;
 #include "../models/DualCortexA7_model.h"
 #include "../models/AXIBus_model.h"
 #include "../models/RAM_model.h"
-#include "models_catalogue.h"
 #include "../models/APBBus_model.h"
 #include "../models/AXIAPB_model.h"
 #include "../models/PL180_model.h"
 #include "../models/LAN9118_model.h"
+#include "UARTSubSystem.h"
 $includes_end;
 
 $module_begin("top");
@@ -31,20 +31,20 @@ $end
 $init("bridge"),
 bridge(0)
 $end
-$init("console0"),
-console0(0)
-$end
 $init("apb"),
 apb(0)
-$end
-$init("uart0"),
-uart0(0)
 $end
 $init("mci"),
 mci(0)
 $end
 $init("eth"),
 eth(0)
+$end
+$init("serial0"),
+serial0(0)
+$end
+$init("serial1"),
+serial1(0)
 $end
     $initialization_end
 {
@@ -61,14 +61,8 @@ $end;
 $create_component("bridge");
 bridge = new AXIAPB_pvt("bridge");
 $end;
-$create_component("console0");
-console0 = new UART_Visualization_Middleware_pvt("console0");
-$end;
 $create_component("apb");
 apb = new APBBus_pvt("apb");
-$end;
-$create_component("uart0");
-uart0 = new UART_PL011_pvt("uart0");
 $end;
 $create_component("mci");
 mci = new PL180_pvt("mci");
@@ -76,38 +70,26 @@ $end;
 $create_component("eth");
 eth = new LAN9118_pvt("eth");
 $end;
+$create_component("serial0");
+serial0 = new UARTSubSystem("serial0");
+$end;
+$create_component("serial1");
+serial1 = new UARTSubSystem("serial1");
+$end;
 $bind("cpu->master0","axi->cpu");
 vista_bind(cpu->master0, axi->cpu);
 $end;
 $bind("axi->peripherals","bridge->slave");
 vista_bind(axi->peripherals, bridge->slave);
 $end;
-$bind("apb->uart0","uart0->AMBA_APB");
-vista_bind(apb->uart0, uart0->AMBA_APB);
-$end;
 $bind("axi->memory","mem->slave");
 vista_bind(axi->memory, mem->slave);
-$end;
-$bind("console0->nUARTRTS","uart0->nUARTCTS");
-vista_bind(console0->nUARTRTS, uart0->nUARTCTS);
-$end;
-$bind("uart0->UARTTXD","console0->UARTRXD");
-vista_bind(uart0->UARTTXD, console0->UARTRXD);
-$end;
-$bind("console0->UARTTXD","uart0->UARTRXD");
-vista_bind(console0->UARTTXD, uart0->UARTRXD);
-$end;
-$bind("uart0->nUARTRTS","console0->nUARTCTS");
-vista_bind(uart0->nUARTRTS, console0->nUARTCTS);
 $end;
 $bind("bridge->master","apb->bridge");
 vista_bind(bridge->master, apb->bridge);
 $end;
 $bind("apb->mci","mci->host");
 vista_bind(apb->mci, mci->host);
-$end;
-$bind("uart0->UARTINTR","cpu->irq_5");
-vista_bind(uart0->UARTINTR, cpu->irq_5);
 $end;
 $bind("mci->irq0","cpu->irq_9");
 vista_bind(mci->irq0, cpu->irq_9);
@@ -120,6 +102,18 @@ vista_bind(axi->ethernet, eth->host);
 $end;
 $bind("eth->irq","cpu->irq_15");
 vista_bind(eth->irq, cpu->irq_15);
+$end;
+$bind("apb->uart1","serial1->AMBA_APB");
+vista_bind(apb->uart1, serial1->AMBA_APB);
+$end;
+$bind("apb->uart0","serial0->AMBA_APB");
+vista_bind(apb->uart0, serial0->AMBA_APB);
+$end;
+$bind("serial1->UARTINTR","cpu->irq_6");
+vista_bind(serial1->UARTINTR, cpu->irq_6);
+$end;
+$bind("serial0->UARTINTR","cpu->irq_5");
+vista_bind(serial0->UARTINTR, cpu->irq_5);
 $end;
     $elaboration_end;
   $vlnv_assign_begin;
@@ -142,20 +136,20 @@ $end;
 $destruct_component("bridge");
 delete bridge; bridge = 0;
 $end;
-$destruct_component("console0");
-delete console0; console0 = 0;
-$end;
 $destruct_component("apb");
 delete apb; apb = 0;
-$end;
-$destruct_component("uart0");
-delete uart0; uart0 = 0;
 $end;
 $destruct_component("mci");
 delete mci; mci = 0;
 $end;
 $destruct_component("eth");
 delete eth; eth = 0;
+$end;
+$destruct_component("serial0");
+delete serial0; serial0 = 0;
+$end;
+$destruct_component("serial1");
+delete serial1; serial1 = 0;
 $end;
     $destructor_end;
   }
@@ -173,20 +167,20 @@ $end;
 $component("bridge");
 AXIAPB_pvt *bridge;
 $end;
-$component("console0");
-UART_Visualization_Middleware_pvt *console0;
-$end;
 $component("apb");
 APBBus_pvt *apb;
-$end;
-$component("uart0");
-UART_PL011_pvt *uart0;
 $end;
 $component("mci");
 PL180_pvt *mci;
 $end;
 $component("eth");
 LAN9118_pvt *eth;
+$end;
+$component("serial0");
+UARTSubSystem *serial0;
+$end;
+$component("serial1");
+UARTSubSystem *serial1;
 $end;
   $fields_end;
   $vlnv_decl_begin;
--- ARM/a7/sw/setup.sh
+++ ARM/a7/sw/setup.sh
@@ -1,4 +1,4 @@
-PATH=./buildroot-2016.05/output/host/usr/bin\:$PATH
+export PATH=`pwd`/buildroot-2016.05/output/host/usr/bin\:$PATH
 export TOOL_CHAIN=$(dirname $(dirname `which arm-linux-gcc`))
 
 export VER_LINUX=4.6.1      # kernel version
