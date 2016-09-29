e38a9ba53a59115c4ccb4cf690a48abda5dfd41a
Jon McDonald <jonmcdonald@gmail.com>
2016-09-29 08:13:48 -0400

    General cleanup and .gitignore additions

---
+++ ARM/a7/hw/trace_factorial.tcl
@@ -0,0 +1,29 @@
+
+trace_attribute top.cpu.PV.cpu0.core.core_state
+trace_attribute top.cpu.PV.cpu1.core.core_state
+
+trace_socket   top.sm.slave
+trace_socket   top.sm.irq
+
+trace_register top.sm.PV.ADR
+trace_register top.sm.PV.CTL
+
+select_core "top.cpu.PV.cpu0.core"
+add_default_symbol_file
+
+insert_tracepoint mainEntry -at-function-entry main -do-raw {
+  printf("Entered main function at %d MS.\n", get_time_stamp()/1000000);
+}
+
+insert_tracepoint mainExit -at-function-exit main -do-raw {
+  printf("Exiting main function at %d MS.\n", get_time_stamp()/1000000);
+  request_shutdown();
+}
+
+select_core "top.cpu.PV.cpu1.core"
+add_default_symbol_file
+
+insert_tracepoint mainEntry -at-function-entry main -do-raw {
+  printf("Stopping core 1 at %d MS.\n", get_time_stamp()/1000000);
+  //request_shutdown();
+}
--- ARM/a7/sw/readme.txt
+++ ARM/a7/sw/readme.txt
@@ -1,3 +1,24 @@
 wget https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.6.1.tar.xz
 wget https://buildroot.org/downloads/buildroot-2016.05.tar.gz
 
+Extract buildroot
+cd buildroot_patches
+  tar cf - . | (cd ../buildroot-2016.05; tar xf -)
+
+cd ../buildroot-2016.05
+  make vista_defconfig
+  make -j10
+
+Now you have root filesystem and cross-compiler, all in output dir
+
+Then cd ..
+./script/build_kernel.sh
+
+source 
+  setup.sh
+  script/setup_kernel.sh
+
+cd boot-wrapper
+  build.sh
+
+parameters.txt references axf and img from sd build
